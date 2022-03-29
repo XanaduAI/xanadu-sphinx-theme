66e678f117480edc1e51c0e0a312155bb496a049
Mikhail Andrenkov <M.Andrenkov@gmail.com>
2022-03-29 14:48:53 -0400

    Remove extra code from `__init__.py`

--- xanadu_sphinx_theme/__init__.py
+++ xanadu_sphinx_theme/__init__.py
@@ -1,41 +1,8 @@
-# Copyright 2019 Xanadu Quantum Technologies Inc.
-
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-
-#     http://www.apache.org/licenses/LICENSE-2.0
-
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-"""Xanadu Sphinx Theme"""
-import os
-
-from .custom_directives import IncludeDirective, GalleryItemDirective, CustomGalleryItemDirective
-from ._version import __version__
-
-
-def get_path():
-    """Shortcut for users whose theme is next to their conf.py"""
-    return os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
-
-
-def update_context(app, pagename, templatename, context, doctree):
-    context["xanadu_version"] = __version__
-
+"""
+This module registers the Xanadu Sphinx Theme. For more information, see
+https://www.sphinx-doc.org/en/master/development/theming.html
+"""
+from os import path
 
 def setup(app):
-    if hasattr(app, "add_html_theme"):
-        theme_path = os.path.abspath(os.path.dirname(__file__))
-        app.add_html_theme("xanadu", theme_path)
-
-    app.connect("html-page-context", update_context)
-    app.add_directive("includenodoc", IncludeDirective)
-    app.add_directive("galleryitem", GalleryItemDirective)
-    app.add_directive("customgalleryitem", CustomGalleryItemDirective)
-    app.add_stylesheet(os.path.join(theme_path, "static/gallery.css"))
-
-    return {"version": __version__, "parallel_read_safe": True}
+    app.add_html_theme("xanadu", path.abspath(path.dirname(__file__)))
