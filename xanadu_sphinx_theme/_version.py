9e03daac1a3f0288533be1ac4c18a7dc0561884d
ashishks0522 <ashishks0522@users.noreply.github.com>
2025-09-02 19:43:30 +0000

    Bump version to `0.18.0-dev`

--- xanadu_sphinx_theme/_version.py
+++ xanadu_sphinx_theme/_version.py
@@ -3,4 +3,4 @@ This module specifies the Xanadu Sphinx Theme version with https://semver.org/
 using the following format: <major>.<minor>.<patch>[-<pre-release>].
 """
 
-__version__ = "0.17.0"
+__version__ = "0.18.0-dev"
